0adf8a76409b85af0452adc7cceacfc1fd0c31bb
lzhig <lzhig@outlook.com>
2018-03-14 15:30:25 +0800

    protobuf for c#
    update tools and build script

--- build/build_proto_csharp.bat
+++ build/build_proto_csharp.bat
@@ -2,4 +2,5 @@ call global.bat
 
 @echo %TOOLS_DIR%
 
-%TOOLS_DIR%\protobuf\protobuf-net\protogen.exe -I%SRC_DIR%\common\protobuf\ --csharp_out=%SRC_DIR%\unity\ msg.proto
+rem %TOOLS_DIR%\protobuf\protobuf-net\protogen.exe -I%SRC_DIR%\common\protobuf\ --csharp_out=%SRC_DIR%\unity\ msg.proto
+%TOOLS_DIR%\protobuf\protoc -I%SRC_DIR%\common\protobuf\ --csharp_out=%SRC_DIR%\unity\  msg.proto
